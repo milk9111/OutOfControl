a22292c03ad8d001b6b001a9cb577df9a2d5e07f
Connor Lundberg <connorlndbrg349@gmail.com>
2020-07-12 12:17:53 -0400

    Updated game to work better on Windows

--- Assets/Scripts/CreditsManager.cs
+++ Assets/Scripts/CreditsManager.cs
@@ -5,6 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class CreditsManager : MonoBehaviour
 {
+    public GameObject terminationLetter;
+
+    public Transform _target;
+
+    public float scrollSpeed;
+
+    public bool rollCredits;
+
+    void Start()
+    {
+        terminationLetter.SetActive(true);
+        rollCredits = false;
+    }
+
+    void Update()
+    {
+        if (!rollCredits)
+        {
+            return;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _target.position, scrollSpeed * Time.deltaTime);
+    }
+
+    public void RollCredits()
+    {
+        rollCredits = true;
+        terminationLetter.SetActive(false);
+    }
+
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
